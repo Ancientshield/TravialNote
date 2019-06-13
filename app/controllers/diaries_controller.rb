0dda67c789ed796e2da048c74edb31f1270b0ec2
Grace <syen741205@gmail.com>
2019-06-13 11:44:22 +0800

    最最基本建立日記及顯示，搭配使用者登入

---
+++ app/controllers/diaries_controller.rb
@@ -0,0 +1,40 @@
+class DiariesController < ApplicationController
+
+  def index
+    @diaries = Diary.where(user_id:current_user)
+
+  end
+
+  def show
+  end
+
+  def new
+    @diary = Diary.new()
+  end
+
+  def create
+    @diary = Diary.new(diary_params)
+    
+    if @diary.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def diary_params
+    params.require(:diary).permit(:content,:location,:diary_date)
+    
+  end
+
+end

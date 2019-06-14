c3af3a5f67c2405c87272a82ed5943156729fdd8
Ancientshield <ancientsword520@gmail.com>
2019-06-14 14:28:34 +0800

    google map view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'diaries#index'
   resources :diaries
   devise_for :users
+  resources :users
+  get "/ajax", to: "users#ajax"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

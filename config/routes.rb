2da12c3e403dd330f489afddef901e685a318ea6
Ancientshield <ancientsword520@gmail.com>
2019-06-20 12:31:16 +0800

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ Rails.application.routes.draw do
   resources :drafts
   resources :pictures
   resources :pockets
-  devise_for :users
   resources :users
+  devise_for :users
   get "/ajax", to: "users#ajax"
-  get "/diaries", to: "diaries#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

b1ea39eb15e1acf49848c978bf1aa0b5a416a553
Ancientshield <46489740+Ancientshield@users.noreply.github.com>
2019-06-24 23:03:17 +0800

    fix routing error
    fix done.

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
   resources :drafts
   resources :pictures
   resources :pockets
-  resources :users
   devise_for :users
+  resources :users
   get "/ajax", to: "users#ajax"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

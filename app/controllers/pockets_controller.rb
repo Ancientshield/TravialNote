194aad7e350ba0e7bc43caa91ec877bd166a3229
Ancientshield <ancientsword520@gmail.com>
2019-06-27 17:40:09 +0800

    ...

--- app/controllers/pockets_controller.rb
+++ app/controllers/pockets_controller.rb
@@ -1,7 +1,27 @@
 class PocketsController < ApplicationController
-    def index
+  before_action :authenticate_user!
+  def index
+    @pocket_lists = PocketList.new
+  end
+
+  def new
+    @pocket = current_user.pocket_lists.new
+    @pocket.location = session[:location]
+  end
+
+  def create
+    @pocket = current_user.pocket_lists.new
+    @pocket.location = session[:location]
+    @pocket.expect_date = params[:pocket_list][:expect_date]
+    if @pocket.save
+      redirect_to pockets_path, notice: "加入口袋成功！"
+    else
+      render :new
     end
-    
-    def new
-    end
+  end
+
+  def set_map_session
+    location_params = params.permit(location: {})
+    session[:location] = location_params[:location]
+  end
 end

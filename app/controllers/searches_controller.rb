1c1b293c0e7979ca7938f71165c47898583d9928
Grace <syen741205@gmail.com>
2019-07-02 11:42:41 +0800

    search feature finished

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,10 +1,30 @@
 class SearchesController < ApplicationController
+  before_action :authenticate_user!
 
-
-  def index
-    search_params
+  def new
+    # search_params
+    @tags = current_user.tags
+  end
+  
+  def show
+    @tags = current_user.tags
+    @diaries = []
+    diary_ids = DiaryTag.where(tag_id: params[:format])
+    
+    diary_ids.each do |diary|
+      @diaries << current_user.diaries.find_by(id: diary.diary_id)
+    end
+    
+    if params[:search]
+      @diaries = current_user.diaries.where('content LIKE ?', "%#{params[:search]}%")
+    end
   end
 
+
+  def create
+    search_params
+    render :new
+  end
   private
 
   def search_params
@@ -12,12 +32,12 @@ class SearchesController < ApplicationController
     # 有搜尋文字
     if params[:search]
       # 執行搜尋，找tag或內容有 關鍵字
-      @tags = current_user.tags.where('label LIKE ?', "%#{params[:search]}%")
+      # @tags = current_user.tags.where('label LIKE ?', "%#{params[:search]}%")
       @diaries = current_user.diaries.where('content LIKE ?', "%#{params[:search]}%")
     else
       # 
       @diaries = current_user.present? ? current_user.diaries : []
-      @tags = current_user.present? ? current_user.tags : []
+      # @tags = current_user.present? ? current_user.tags : []
     end
   end
 
